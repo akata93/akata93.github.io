f83550ffaf877ca949de9225aebc1f8ee63565c4
akata93 <hugo.acata@gmail.com>
2016-03-16 10:25:08 -0600

    Update 1603-textura.js

--- rv/1603-textura.js
+++ rv/1603-textura.js
@@ -1,6 +1,6 @@
 function setup(){
   THREE.ImageUtils.crossOrigin='';
-  var textura = THREE.loadTexture("http://three.js.org/examples/textures/crate.gif);
+  var textura = THREE.loadTexture('http://three.js.org/examples/textures/crate.gif');
   var material = new THREE.MeshBasicMaterial({map:textura})}
   var forma = new THREE.BoxGeometry(1,1,1)
   malla= new THREE.Mesh (forma, material)
