9d0d04b6414eedb12269c3ddfacdc799fbea43a1
akata93 <hugo.acata@gmail.com>
2016-03-16 10:58:57 -0600

    Update textura-4.js

--- rv/textura-4.js
+++ rv/textura-4.js
@@ -2,7 +2,7 @@ function setup(){
   THREE.ImageUtils.crossOrigin='';
   var textura = THREE.ImageUtils.loadTexture('http://threejs.org/examples/textures/cube/pisa/nz.png');
   var material = new THREE.MeshPhongMaterial({ map: textura});
-  var forma = new THREE.BoxGeometry(1,1,1);
+  var forma = new THREE.SphereGeometry(1,1,1);
   malla= new THREE.Mesh (forma, material);
   
   var luzPuntual = new THREE.PointLight(0xffffff);
