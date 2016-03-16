783458d23bc14b98ad406fcd0d558eba8a5ec3ee
akata93 <hugo.acata@gmail.com>
2016-03-16 11:01:37 -0600

    Create textura-5.js

---
+++ rv/textura-5.js
@@ -0,0 +1,34 @@
+function setup(){
+  THREE.ImageUtils.crossOrigin='';
+  var textura = THREE.ImageUtils.loadTexture('http://threejs.org/examples/textures/planets/earth_atmos_2048.jpg');
+  var material = new THREE.MeshPhongMaterial({ map: textura});
+  var forma = new THREE.SphereGeometry(1,1,1);
+  malla= new THREE.Mesh (forma, material);
+  
+  var luzPuntual = new THREE.PointLight(0xffffff);
+  luzPuntual.position.x=10;
+  luzPuntual.position.y=10;
+  luzPuntual.position.z=10;
+  
+  escena = new THREE.Scene();
+  escena.add(malla);
+  escena.add(luzPuntual);
+  
+  camara= new THREE.PerspectiveCamera();
+  camara.position.z=5;
+
+  renderer= new THREE.WebGLRenderer();
+  renderer.setSize(window.innerHeight*0.95, window.innerHeight*0.95);
+  document.body.appendChild(renderer.domElement);
+}
+
+function loop(){
+  requestAnimationFrame(loop);
+  malla.rotation.z+=0.001;
+  malla.rotation.y+=0.01;
+  renderer.render(escena, camara);
+}
+var camara, escena, renderer, malla ;
+
+setup();
+loop();
