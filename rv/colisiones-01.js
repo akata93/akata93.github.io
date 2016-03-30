b80d158921c048868eae695d3171f8ce9c95ccca
akata93 <hugo.acata@gmail.com>
2016-03-30 10:28:21 -0600

    Update colisiones-01.js

--- rv/colisiones-01.js
+++ rv/colisiones-01.js
@@ -1,5 +1,7 @@
 function setup() {
-  cubo1
-  
-  
+  cubo1=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),
+                       new THREE.MeshNormalMaterial());
+  cubo2=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),
+                       new THREE.MeshNormalMaterial());
+  cubo1.position.x=0.7;
 }
