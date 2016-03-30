ed15edd6f87a4e323d3a12a58a4fbd1c8b438b14
akata93 <hugo.acata@gmail.com>
2016-03-30 10:35:59 -0600

    Update colisiones-01.js

--- rv/colisiones-01.js
+++ rv/colisiones-01.js
@@ -4,4 +4,39 @@ function setup() {
   cubo2=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),
                        new THREE.MeshNormalMaterial());
   cubo1.position.x=0.7;
+  cubo2.position.x=-0.7;
+  
+  camara=new THREE.PerspectiveCamera();
+  camara.position.z=5;
+  
+  raycaster= new THREE.Raycaster();
+  rascaster.setFromCamera(new THREE.Vector2(0,0), camara);
+  
+  escena=new THREE.Scene();
+  escena.add(cubo1);
+  escena.add(cubo2);
+  escena.add(camara);
+  
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerHeight*0.95,window.innerHeight*0.95);
+  document.body.appendChild(renderer.domElement);
+  step=0.01;
 }
+function loop(){
+  var intersects = raycaster.intersectObjects(escena.children);
+  if (intersects.lenght>0) step=-step;
+  
+  cubo1.rotation.x+= step;
+  cubo1.rotation.y+= step;
+  cubo2.rotation.x+= step;
+  cubo2.rotation.y+= step;
+  
+  renderer.render(escena,camara);
+  requestAnimationFrame(loop);
+  
+}
+var cubo1,cubo2,escena,camara,renderer;
+var raycaster,step;
+
+setup();
+loop();
