b69aabe77239305cfd0512497d08e4b3ce7b2769
akata93 <hugo.acata@gmail.com>
2016-03-30 11:27:18 -0600

    Update colisiones-02.js

--- rv/colisiones-02.js
+++ rv/colisiones-02.js
@@ -35,9 +35,9 @@ function loop(){
   obstaculo2 = raycaster2.intersectObject(pelota1);
   obstaculo3 = raycaster3.intersectObject(cubo2);
   obstaculo4 = raycaster4.intersectObject(pelota2);
-  obstaculo5 = raycaster1.intersectObject(cubo2);
+  obstaculo5 = raycaster2.intersectObject(cubo2);
   //obstaculo5 = raycaster2.intersectObject(pelota2);
-  obstaculo6 = raycaster3.intersectObject(cubo1);
+  obstaculo6 = raycaster4.intersectObject(cubo1);
   //obstaculo7 = raycaster4.intersectObject(pelota1);
   
   if ((obstaculo1.length>0 && (obstaculo1[0].distance <=0.5)) || (obstaculo2.length>0 && (obstaculo2[0].distance <=0.1))
