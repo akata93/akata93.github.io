4e43e6729c47ca8500da8eead83f93df70c3f476
akata93 <hugo.acata@gmail.com>
2016-03-30 11:21:20 -0600

    Update colisiones-02.js

--- rv/colisiones-02.js
+++ rv/colisiones-02.js
@@ -31,21 +31,18 @@ function setup() {
   step=0.1;
 }
 function loop(){
-  obstaculo1 = raycaster1.intersectObject(cubo2);
+  obstaculo1 = raycaster1.intersectObject(cubo1);
   obstaculo2 = raycaster2.intersectObject(pelota1);
-  obstaculo3 = raycaster3.intersectObject(cubo1);
+  obstaculo3 = raycaster3.intersectObject(cubo2);
   obstaculo4 = raycaster4.intersectObject(pelota2);
   
-  if ((obstaculo1.length>0 && (obstaculo1[0].distance <=0.5)) || (obstaculo2.length>0 && (obstaculo2[0].distance <=0.5))
-  || (obstaculo3.length>0 && (obstaculo3[0].distance <=0.5))|| (obstaculo4.length>0 && (obstaculo4[0].distance <=0.5)))
+  if ((obstaculo1.length>0 && (obstaculo1[0].distance <=0.5)) || (obstaculo2.length>0 && (obstaculo2[0].distance <=0.1))
+  || (obstaculo3.length>0 && (obstaculo3[0].distance <=0.5))|| (obstaculo4.length>0 && (obstaculo4[0].distance <=0.1)))
   step=-step;
   
   pelota1.position.x+=step;
   pelota2.position.x+=-step;
-  raycaster1.set(pelota1.position, new THREE.Vector3(1,0,0));
-  raycaster2.set(pelota1.position, new THREE.Vector3(-1,0,0));
-  raycaster3.set(pelota2.position, new THREE.Vector3(1,0,0));
-  raycaster4.set(pelota2.position, new THREE.Vector3(-1,0,0));
+  
   renderer.render(escena,camara);
   requestAnimationFrame(loop);
   
