15a176b054d3c574ee651118a8ba469e55306ffb
akata93 <hugo.acata@gmail.com>
2016-04-06 20:19:14 -0500

    Update cuarto.js

--- cuarto.js
+++ cuarto.js
@@ -46,7 +46,7 @@ escena.add(luzPuntual2);
 
 camara=new THREE.PerspectiveCamera();
 //camara.rotation.x = 90 * Math.PI / 180;
-camara.position.z=1200;
+camara.position.z=1500;
 camara.position.y = 500 ;  
 //camara.position.y=;
 
