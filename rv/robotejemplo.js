d01c2e7238b0717c4a107e59d1e1845f98efa120
akata93 <hugo.acata@gmail.com>
2016-04-13 10:24:11 -0500

    Update robotejemplo.js

--- rv/robotejemplo.js
+++ rv/robotejemplo.js
@@ -1,1 +1,29 @@
 
+Robot.prototype.act= function (environment){
+  var command= this.actuator.commands.pop();
+  console.los('Undefined command');
+  else if (command in this operations)
+  this.operations[command](this);
+  else
+  console.log('Unknown command');
+};
+
+Robot.prototype.operations()={ };
+Robot.prototype.operations.goStraight= function(robot,distance){
+  if (distance===undefined)
+  distance=0.05;
+  robot.position.x+=distance*Math.cos(robot.rotation.z);
+  robot.position.y=distance*Math.sinbot.rotation.z):
+};
+
+Robot.prototype.operations.rotateCW= function(robot,angle){
+  if (angle===undefined)
+  angle=-Math.PI/2;
+  robot.rotation.z+=angle;
+};
+
+Robot.prototype.operations.rotateCCW= function(robot,angle){
+  if (angle===undefined)
+  angle=Math.PI/2;
+  robot.rotation.z+=angle;
+};
