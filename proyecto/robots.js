5c7ce974e4ee329221b9ffb0a848756b50a014a0
akata93 <hugo.acata@gmail.com>
2016-05-29 19:35:11 -0500

    Update robots.js

--- proyecto/robots.js
+++ proyecto/robots.js
@@ -49,7 +49,7 @@ function Robot(size,x,y){
 	THREE.GeometryUtils.merge(forma,formabasepie);
 	THREE.GeometryUtils.merge(forma,mallabrazo1);
 	THREE.GeometryUtils.merge(forma,mallabrazo2);
-	forma.scale.x = forma.scale.y = forma.scale.z = 0.006;
+	forma.scale.x = forma.scale.y = forma.scale.z = 0.001;
 	//forma.updateMatrix();
 
 	this.sensor=new Sensor();
